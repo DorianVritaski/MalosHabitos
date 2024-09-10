b4361242b1a02ec7915bb9abe9aac8c927d7b649
DorianVritaski <72947488@continetal.edu.pe>
2024-09-09 19:04:17 -0500

    Refactorizacion Ejemplo 1

---
+++ Ejemploo1.py
@@ -0,0 +1,11 @@
+def multiplicacion(multiplicando, multiplicador):
+    producto = multiplicando * multiplicador
+    return producto
+
+if __name__ == "__main__":
+
+    multiplicando = float(input("Multiplicadondo: "))
+    multiplicador = float(input("Multiplicador: "))
+
+    resultado = multiplicacion(multiplicando, multiplicador)
+    print(f"{multiplicando} * {multiplicador} = {resultado}")
